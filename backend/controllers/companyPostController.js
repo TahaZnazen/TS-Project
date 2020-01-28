76e85202603ac9758d9e765939de44f48dbcdba0
Taha_Znazen <48031507+blusk1n@users.noreply.github.com>
2020-01-28 19:53:32 +0100

    Update companyPostController.js

--- backend/controllers/companyPostController.js
+++ backend/controllers/companyPostController.js
@@ -10,7 +10,6 @@ exports.addPost = async (req, res) => {
   } catch (err) {
     res.json({ err });
   }
-  
 };
 exports.findOne = id => {
   CompanyPost.find({ _id: id });
@@ -19,7 +18,6 @@ exports.findOne = id => {
 exports.findAndDelete = async (req, res) => {
   CompanyPost.findByIdAndRemove(req.params.id).then(() => "List deleted");
 };
-
 exports.findAndUpdate = async (req, res) => {
   try {
     const id = req.params.id;
@@ -30,7 +28,6 @@ exports.findAndUpdate = async (req, res) => {
   } catch (err) {
     res.json({ err });
   }
-  
 };
 exports.findAll = id => {
   return CompanyPost.find({ _id: id });
