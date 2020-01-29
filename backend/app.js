1bc233caec3b030dac1011cea518df589279760d
blusk1n <tahaznazen@gmail.com>
2020-01-29 10:06:02 +0100

    pulled from farouk work

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,16 @@
-const express = require('express')
-const morgan = require ('morgan')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
-const userRouter = require('./routes/userRouter')
+const userRouter = require("./routes/userRouter");
 
-
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cors())
-
-
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
 
 //ROUTES
 
-app.use('/api/v1/users',userRouter)
+app.use("/api/v1/users", userRouter);
 
-module.exports = app
+module.exports = app;
