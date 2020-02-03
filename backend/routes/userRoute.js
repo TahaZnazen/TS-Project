1065911882448b7a3015656d303bd3e9f89e8b82
Hamza <hamzadanden2@gmail.com>
2020-02-03 19:12:23 +0100

    user upload img

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,4 +7,7 @@ router.route("/").get(userController.findAllUsers);
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.route("/:id").get(userController.findOneUser);
+router
+  .route("/update/:id")
+  .patch(userController.uploadUserPhoto, userController.updateUser);
 module.exports = router;
