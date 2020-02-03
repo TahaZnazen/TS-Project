f85bdfa6f88c1d4c32ac20a56f369987e8067393
blusk1n <tahaznazen@gmail.com>
2020-02-03 19:44:07 +0100

    conflict solved

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,16 +5,15 @@ import App from "./App";
 
 import { Provider } from "react-redux";
 import { createStore, combineReducers } from "redux";
-
-
-/////////////////////////
 import store from "./store";
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+// /////////////////////////
+// import store from "./store";
+// import { createStore, combineReducers, applyMiddleware } from "redux";
+// import thunk from "redux-thunk";
 
-import reducers from "./reducers";
-/////////
-const store = createStore(reducers, applyMiddleware(thunk));
+// import reducers from "./reducers";
+// /////////
+// const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
