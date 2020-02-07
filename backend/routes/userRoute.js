04ac578bd83c779446e973aa1085e1b6955938f5
Bilel <Bilel@MacBook-Pro-de-Snake.local>
2020-02-07 17:47:27 +0100

    get img pull

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -13,10 +13,7 @@ router.route("/getjobs/:id").get(userController.findJobs);
 router
   .route("/update/:id")
   .patch(userController.uploadUserPhoto, userController.updateUser);
-<<<<<<< HEAD
-=======
 router.route("/image/:id").get(userController.getimg);
 router.route("/updatePassword/:id").patch(userController.updatePassword);
->>>>>>> f93c75b3796ac9e9c0b72b370c24a0c69077132e
 
 module.exports = router;
