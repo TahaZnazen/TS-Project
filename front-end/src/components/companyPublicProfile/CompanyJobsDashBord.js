33c9a857da2ee0064a242d1a3874fff2b687b6de
blusk1n <tahaznazen@gmail.com>
2020-02-13 02:22:04 +0100

    the company table work , seeing candidates profile done , company profile done ,

--- front-end/src/components/companyPublicProfile/CompanyJobsDashBord.js
+++ front-end/src/components/companyPublicProfile/CompanyJobsDashBord.js
@@ -4,6 +4,7 @@ import "./companyDashboardProfile.css";
 import { connect } from "react-redux";
 import axios from "axios";
 import { getCompanyJobs } from "../../actions/offersAction";
+import CandidatesModal from "./CandidatesModal";
 class CompanyJobsDashBord extends Component {
   state = {
     idUser: "",
@@ -15,11 +16,15 @@ class CompanyJobsDashBord extends Component {
     axios
       .post("http://localhost:8080/api/users/generateID", { token: token })
       //invoke he function that will get the posts for this company
-      .then(res => this.props.getCompanyJobs(res.data.id))
+      .then(res => {
+        this.props.getCompanyJobs(res.data.id);
+      })
+
       .catch(err => console.log(err));
   }
 
   getID(e) {
+    console.log(e.target.id);
     this.setState({
       idOffre: e.target.id
     });
@@ -29,6 +34,7 @@ class CompanyJobsDashBord extends Component {
       .catch(err => console.log(err));
     console.log(this.state.currentPost);
   }
+
   render() {
     return (
       <div>
@@ -76,29 +82,101 @@ class CompanyJobsDashBord extends Component {
                   width: "100%"
                 }}
               >
-                {[1, 2, 3, 4, 5, 6].map(elm => (
-                  <div className="jobDescrpPlace mb-1">
-                    <div className="card-body" style={{ height: "10vh" }}>
-                      <h3 className="card-title text-center">job title</h3>
+                {this.props.posts[0] &&
+                  this.props.posts[0]["Offers"].map(elm => (
+                    <div className="jobDescrpPlace mb-1">
+                      <div
+                        id={elm._id}
+                        onClick={this.getID.bind(this)}
+                        className="card-body"
+                        style={{ height: "10vh" }}
+                      >
+                        <h3
+                          style={{
+                            pointerEvents: "none"
+                          }}
+                          className="card-title text-center"
+                        >
+                          {elm.title}
+                        </h3>
+                      </div>
+                      <div
+                        style={{
+                          pointerEvents: "none"
+                        }}
+                        className="card-footer text-right "
+                        style={{ backgroundColor: "	#20a8d8" }}
+                      >
+                        <small
+                          style={{ color: "white", pointerEvents: "none" }}
+                        >
+                          {elm.createdAt.replace("T", " ").slice(0, 16)}
+                        </small>
+                      </div>
                     </div>
-                    <div
-                      className="card-footer text-right "
-                      style={{ backgroundColor: "	#20a8d8" }}
-                    >
-                      <small style={{ color: "white" }}>
-                        Last updated 3 mins ago
-                      </small>
-                    </div>
-                  </div>
-                ))}
+                  ))}
               </div>
             </div>
             <div
               className="jobDescriptionDashboard d-flex-column"
-              style={{ width: "70%", height: "90vh" }}
+              style={{ width: "70%", height: "90vh", padding: "10px" }}
             >
-              <h1 className="mt-4">COMPANY NAME</h1>
-              <div className="jobInformation"></div>
+              {/* this one need to be populated with the condidates */}
+              <CandidatesModal
+                candidates={
+                  this.props.posts[0] &&
+                  this.props.posts[0]["Offers"][0].candidates
+                }
+                style={{ position: "absolute" }}
+              />
+              <h1>
+                {this.props.posts[0] &&
+                  "COMPANY : " + this.props.posts[0].Company.name.toUpperCase()}
+              </h1>
+              <div className="jobInformation">
+                <h1 className="text-left ml-2 mt-3">
+                  {this.state.currentPost.title &&
+                    "JOB TITLE : " + this.state.currentPost.title}
+                </h1>
+                <h5 className="text-left ml-2 mt-3">
+                  {this.state.currentPost.location &&
+                    "LOCATION : " + this.state.currentPost.location}
+                </h5>
+                <h1 className="text-right mr-2 mt-3 ">
+                  {this.state.currentPost.title &&
+                    "JOB Type : " + this.state.currentPost.jobType}
+                </h1>
+                <h4 className="text-right mr-2 mt-3 ">
+                  {this.state.currentPost.salaryMin &&
+                    "SALARY : " +
+                      this.state.currentPost.salaryMin +
+                      " -> " +
+                      this.state.currentPost.salaryMax}
+                </h4>
+                <h4
+                  style={{ position: "absolute", bottom: "0", width: "100%" }}
+                  className="text-center mr-2 mt-3 "
+                >
+                  {this.state.currentPost.createdAt &&
+                    "FROM : " +
+                      this.state.currentPost.createdAt
+                        .replace("T", " ")
+                        .slice(0, 16)
+                        .replace(" ", " -AT- ") +
+                      " -TO- " +
+                      this.state.currentPost.expirationDate
+                        .replace("T", " ")
+                        .slice(0, 16)
+                        .replace(" ", " -AT- ")}
+                </h4>
+                <div className="jobDescription">
+                  {this.state.currentPost.description && (
+                    <div className="nestedJobDescription">
+                      <h4>{this.state.currentPost.description}</h4>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
